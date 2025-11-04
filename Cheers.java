b9fb283a359df9fb430bbd8423c1931297b69f70
YoadKlein <yoadklein@icloud.com>
2025-11-04 18:36:09 +0200

    v0.02

--- Cheers.java
+++ Cheers.java
@@ -11,21 +11,23 @@ public class Cheers {
                 String name = args[0];
                 int numOfCheers = Integer.parseInt(args[1]);
 
+                name = name.toUpperCase();
+
                 int i = 0;
                 int j = 0;
 
-                String vowelString = "{AEFHILMNORSXaefhilmnorsx";
+                String vowelString = "AEFHILMNORSX";
 
                 while (i < name.length()){
                         
-                        char a = name.charAt(i);
+                        char a = (name.charAt(i));
                         
                         if (vowelString.indexOf(a) != -1){
-                                System.out.println("Give me an " + name.charAt(i) + ": " + name.charAt(i) + "!");
+                                System.out.println("Give me an " + a + ": " + name.charAt(i) + "!");
 
                         }
                         else {
-                                System.out.println("Give me a  " + name.charAt(i) + ": " + name.charAt(i) + "!");
+                                System.out.println("Give me a  " + a + ": " + name.charAt(i) + "!");
                         }
                         i++;
                 }
