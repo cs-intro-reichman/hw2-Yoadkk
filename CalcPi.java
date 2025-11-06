d96cf57f54bdf32e5b0ac4fe41ec67da1f85cc48
YoadKlein <yoadklein@icloud.com>
2025-11-06 18:49:39 +0200

    CalcPi.java

--- CalcPi.java
+++ CalcPi.java
@@ -2,26 +2,35 @@
 
 public class CalcPi {
 	public static void main(String [] args) { 
-
-		int n = Integer.parseInt(args[0]);
+		int n = (Integer.parseInt(args[0]));
 		double sumOfqPie = 1.0;
 
 	
 
-		for (double i = 1; i < n ; i = i+ 2){
+		for (double i = 1; i < n ; i++){
 			
 
 
 			if (i%4 == 3){
-				double posNum = (i+2);
+				double posNum = (i+4);
 				sumOfqPie += 1/(posNum);
 
 			}
-			if (i%4 != 3){
+			else if (i%4 == 1){
 				double negNum = (i+2);
 				sumOfqPie -= 1/(negNum);
 
 			}
+			else if (i%4 == 2){
+				double posNum = (i+3);
+				sumOfqPie += 1/(posNum);
+
+			}
+			else {
+			double negNum = (i+5);
+			sumOfqPie -= 1/(negNum);
+			}
+
 			
 
 
